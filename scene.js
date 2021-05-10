acbb54444fde8b4909947b3c174a58a59557c37b
Aleix <aleix.costa2@gmail.com>
2021-05-10 17:41:16 +0200

    canvis

--- scene.js
+++ scene.js
@@ -4,6 +4,10 @@ const BUB_STAND_LEFT = 0;
 const BUB_STAND_RIGHT = 1;
 const BUB_WALK_LEFT = 2;
 const BUB_WALK_RIGHT = 3;
+const BUB_WALK_LEFT_SHOOT = 4; 
+const BUB_WALK_RIGHT_SHOOT = 5; 
+const BUB_SHOOT_LEFT = 6; 
+const BUB_SHOOT_RIGHT = 7; 
 
 // Scene. Updates and draws a single scene of the game.
 
@@ -35,7 +39,17 @@ function Scene()
 	this.bubSprite.addKeyframe(BUB_WALK_RIGHT, [32, 32, 32, 32]);
 	this.bubSprite.addKeyframe(BUB_WALK_RIGHT, [32, 64, 32, 32]);
 
+	/*this.bubSprite.addAnimation();
+	this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [0, 0, 32, 32]);
+	this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [32, 96, 64, 128]);
+	this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [0, 32, 32, 64]);
+	this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [32, 96, 64, 128]);
+	this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [0, 64, 32, 96]);
+	this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [32, 96, 64, 128]);*/
+
 	this.bubSprite.setAnimation(BUB_STAND_RIGHT);
+
+
 
 	// Prepare bubble sprite & its animation
 	this.bubbleSprite = new Sprite(400, 160, 32, 32, 3, bubble);
@@ -81,7 +95,30 @@ function Scene(level){
 		this.bubSprite.addKeyframe(BUB_WALK_RIGHT, [32, 32, 32, 32]);
 		this.bubSprite.addKeyframe(BUB_WALK_RIGHT, [32, 64, 32, 32]);
 
-		this.bubSprite.setAnimation(BUB_STAND_RIGHT);
+		this.bubSprite.addAnimation();
+		this.bubSprite.addKeyframe(BUB_WALK_LEFT_SHOOT, [0, 0, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_WALK_LEFT_SHOOT, [0, 32, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_WALK_LEFT_SHOOT, [0, 96, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_WALK_LEFT_SHOOT, [0, 64, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_WALK_LEFT_SHOOT, [32, 96, 32, 32]);
+
+		this.bubSprite.addAnimation();
+		this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [32, 0, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [32, 32, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [0, 96, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [32, 64, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_WALK_RIGHT_SHOOT, [32, 96, 32, 32]);
+
+		this.bubSprite.addAnimation();
+		this.bubSprite.addKeyframe(BUB_SHOOT_LEFT, [0, 96, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_SHOOT_LEFT, [32, 96, 32, 32]);
+
+		this.bubSprite.addAnimation();
+		this.bubSprite.addKeyframe(BUB_SHOOT_RIGHT, [64, 96, 32, 32]);
+		this.bubSprite.addKeyframe(BUB_SHOOT_RIGHT, [96, 96, 32, 32]);
+		
+		
+		this.bubSprite.setAnimation(BUB_STAND_LEFT);
 
 		// Prepare bubble sprite & its animation
 		this.bubbleSprite = new Sprite(400, 160, 32, 32, 3, bubble);
@@ -117,11 +154,24 @@ Scene.prototype.update = function(deltaTime, level)
 		if(this.bubSprite.x < 478)
 			this.bubSprite.x += 2;
 	}
+	else if(keyboard[32]) // KEY_SHOOT
+	{
+		if(this.bubSprite.currentAnimation != BUB_SHOOT_LEFT && this.bubSprite.currentAnimation == BUB_STAND_LEFT)
+			this.bubSprite.setAnimation(BUB_SHOOT_LEFT);
+		if(this.bubSprite.currentAnimation != BUB_SHOOT_RIGHT && this.bubSprite.currentAnimation == BUB_STAND_RIGHT)
+			this.bubSprite.setAnimation(BUB_SHOOT_RIGHT);	
+		if(this.bubSprite.x >= 2)
+			this.bubSprite.x -= 0;
+	}
 	else
 	{
 		if(this.bubSprite.currentAnimation == BUB_WALK_LEFT)
 			this.bubSprite.setAnimation(BUB_STAND_LEFT);
 		if(this.bubSprite.currentAnimation == BUB_WALK_RIGHT)
+			this.bubSprite.setAnimation(BUB_STAND_RIGHT);
+		if(this.bubSprite.currentAnimation == BUB_SHOOT_LEFT)
+			this.bubSprite.setAnimation(BUB_STAND_LEFT);
+		if(this.bubSprite.currentAnimation == BUB_SHOOT_RIGHT)
 			this.bubSprite.setAnimation(BUB_STAND_RIGHT);
 	}
 
